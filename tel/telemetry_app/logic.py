3989f84798c7b11278df6cf6760fc5c0f6eb76c1
email <ht555@exeter.ac.uk>
2026-01-17 17:53:04 +0000

    added my telemetry objects

--- tel/telemetry_app/logic.py
+++ tel/telemetry_app/logic.py
@@ -103,6 +103,119 @@ class BossEncounterComplete:
         self.encounter_name = encounter_name
         self.stage_number = stage_number
         self.player_HP_remaining = player_HP_remaining
+        
+class BossEncounterFail:
+    def __init__(
+            self,
+            userID: int,
+            sessionID: int,
+            timestamp: str,
+            encounter_name: EncounterName,
+            stage_number: int
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.stage_number = stage_number
+
+class BossEncounterRetry:
+    def __init__(
+            self,
+            userID: int,
+            sessionID: int,
+            timestamp: str,
+            encounter_name: EncounterName,
+            stage_number: int,
+            lives_left: int
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.stage_number = stage_number
+        self.lives_left = lives_left
+
+class GainCoin:
+    def __init__(
+            self,
+            userID: int,
+            sessionID: int,
+            timestamp: str,
+            encounter_name: EncounterName,
+            stage_number: int,
+            coins_gained: int
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.stage_number = stage_number
+        self.coins_gained = coins_gained
+
+class BuyUpgrade:
+    def __init__(
+            self,
+            userID: int,
+            sessionID: int,
+            timestamp: str,
+            encounter_name: EncounterName,
+            stage_number: int,
+            coins_spent: int,
+            upgrade_bought: UpgradeName
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.stage_number = stage_number
+        self.coins_spent = coins_spent
+        self.upgrade_bought = upgrade_bought
+
+class EndSession:
+    def __init__(
+            self,
+            userID: int,
+            sessionID: int,
+            timestamp: str
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+
+class SettingsChange:
+    def __init__(
+        self,
+        userID: int,
+        sessionID: int,
+        timestamp: str,
+        encounter_name: EncounterName,
+        setting: SettingName,
+        value: int
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.setting = setting
+        self.value = value
+
+class KillEnemy:
+    def __init__(
+        self,
+        userID: int,
+        sessionID: int,
+        timestamp: str,
+        encounter_name: EncounterName,
+        stage_number: int,
+        enemy_type: EnemyType
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.stage_number = stage_number
+        self.enemy_type = enemy_type
 
 class StartTelemetry:
     def __init__(self,
