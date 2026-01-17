419eae738d82677c470a9251e7889471380ac97e
Emre Acarsoy <dev@eacarsoy.com>
2026-01-17 16:05:34 +0000

    formatting

--- tel/events.py
+++ tel/events.py
@@ -1,46 +1,46 @@
 from enum import Enum
 
 class EventType(str, Enum):
-    StartTelemetry = "StartTelemetry"
-    SessionStart = "SessionStart"
-    EndSession = "EndSession"
+    START_TELEMETRY = "StartTelemetry"
+    SESSION_START = "SessionStart"
+    END_SESSION = "EndSession"
 
-    NormalEncounterStart = "NormalEncounterStart"
-    NormalEncounterComplete = "NormalEncounterComplete"
-    NormalEncounterFail = "NormalEncounterFail"
-    NormalEncounterRetry = "NormalEncounterRetry"
+    NORMAL_ENCOUNTER_START = "NormalEncounterStart"
+    NORMAL_ENCOUNTER_COMPLETE = "NormalEncounterComplete"
+    NORMAL_ENCOUNTER_FAIL = "NormalEncounterFail"
+    NORMAL_ENCOUNTER_RETRY = "NormalEncounterRetry"
 
-    BossEncounterStart = "BossEncounterStart"
-    BossEncounterComplete = "BossEncounterComplete"
-    BossEncounterFail = "BossEncounterFail"
-    BossEncounterRetry = "BossEncounterRetry"
+    BOSS_ENCOUNTER_START = "BossEncounterStart"
+    BOSS_ENCOUNTER_COMPLETE = "BossEncounterComplete"
+    BOSS_ENCOUNTER_FAIL = "BOSS_ENCOUNTER_RETRY"
+    BOSS_ENCOUNTER_RETRY = "BossEncounterRetry"
 
-    GainCoin = "GainCoin"
-    BuyUpgrade = "BuyUpgrade"
-    SettingsChange = "SettingsChange"
-    KillEnemy = "KillEnemy"
+    GAIN_COIN = "GainCoin"
+    BUY_UPGRADE = "BuyUpgrade"
+    SETTINGS_CHANGE = "SettingsChange"
+    KILL_ENEMY = "KillEnemy"
 
 
 # TODO: To be updated when encounter names are defined.
 class EncounterName(str, Enum):
-    Encounter1 = "ENCOUNTER_1"
-    Encounter2 = "ENCOUNTER_2"
-    Boss1 = "BOSS_1"
+    ENCOUNTER_1 = "ENCOUNTER_1"
+    ENCOUNTER_2 = "ENCOUNTER_2"
+    BOSS_1 = "BOSS_1"
 
 
 # TODO: To be updated when upgrade names are defined.
 class UpgradeName(str, Enum):
-    Upgrade1 = "UPGRADE_1"
-    Upgrade2 = "UPGRADE_2"
+    UPGRADE_1 = "UPGRADE_1"
+    UPGRADE_2 = "UPGRADE_2"
 
 
 # TODO: To be updated when enemy names are defined.
 class EnemyType(str, Enum):
-    EnemyType1 = "ENEMY_TYPE_1"
-    EnemyType2 = "ENEMY_TYPE_2"
-    EnemyType3 = "ENEMY_TYPE_3"
+    ENEMY_TYPE_1 = "ENEMY_TYPE_1"
+    ENEMY_TYPE_2 = "ENEMY_TYPE_2"
+    ENEMY_TYPE_3 = "ENEMY_TYPE_3"
 
 # TODO: To be updated when settings names are defined.
 class SettingName(str, Enum):
-    Setting1 = "SETTING_1"
-    Setting2 = "SETTING_2"
+    SETTING_1 = "SETTING_1"
+    SETTING_2 = "SETTING_2"
