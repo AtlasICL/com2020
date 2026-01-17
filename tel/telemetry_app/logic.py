59cf24919d4d37358a3c412467147c5f13f9fbf2
Emre Acarsoy <dev@eacarsoy.com>
2026-01-17 17:44:36 +0000

    wip

--- tel/telemetry_app/logic.py
+++ tel/telemetry_app/logic.py
@@ -1,6 +1,111 @@
 import json
+from typing import TypeAlias
 
-from tel.telemetry_app.events import EventType, EncounterName, UpgradeName, EnemyType, SettingName
+from events import EventType, EncounterName, UpgradeName, EnemyType, SettingName
+
+class SessionStart:
+    def __init__(
+            self,
+            userID: int,
+            sessionID: int,
+            timestamp: str
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+
+class NormalEncounterStart:
+    def __init__(self,
+            userID: int,
+            sessionID: int,
+            timestamp: str,
+            encounter_name: EncounterName,
+            stage_number: int
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.stage_number = stage_number
+
+class NormalEncounterComplete:
+    def __init__(self,
+            userID: int,
+            sessionID: int,
+            timestamp: str,
+            encounter_name: EncounterName,
+            stage_number: int,
+            player_HP_remaining: int
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.stage_number = stage_number
+        self.player_HP_remaining = player_HP_remaining
+
+class NormalEncounterFail:
+    def __init__(self,
+            userID: int,
+            sessionID: int,
+            timestamp: str,
+            encounter_name: EncounterName,
+            stage_number: int
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.stage_number = stage_number
+
+class NormalEncounterRetry:
+    def __init__(self,
+            userID: int,
+            sessionID: int,
+            timestamp: str,
+            encounter_name: EncounterName,
+            stage_number: int,
+            lives_left: int
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.stage_number = stage_number
+        self.lives_left = lives_left
+
+class BossEncounterStart:
+    def __init__(self,
+            userID: int,
+            sessionID: int,
+            timestamp: str,
+            encounter_name: EncounterName,
+            stage_number: int
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.stage_number = stage_number
+
+class BossEncounterComplete:
+    def __init__(self,
+            userID: int,
+            sessionID: int,
+            timestamp: str,
+            encounter_name: EncounterName,
+            stage_number: int,
+            player_HP_remaining: int
+    ):
+        self.userID = userID
+        self.sessionID = sessionID
+        self.timestamp = timestamp
+        self.encounter_name = encounter_name
+        self.stage_number = stage_number
+        self.player_HP_remaining = player_HP_remaining
+
+
+ValidEvent: TypeAlias = SessionStart | NormalEncounterStart | NormalEncounterComplete | NormalEncounterFail | NormalEncounterRetry | BossEncounterStart | BossEncounterComplete | BossEncounterFail | BossEncounterRetry | GainCoin | BuyUpgrade | EndSession | SettingsChange | KillEnemy | StartTelemetry 
 
 def parse_file(filename: str):
     try: 
@@ -10,5 +115,14 @@ def parse_file(filename: str):
         raise FileNotFoundError(f"Could not find file: {filename}")
     except json.JSONDecodeError:
         raise RuntimeError(f"Could not parse - invalid json.")
-    
 
+def parse_event(event: dict) -> ValidEvent:
+    return StartTelemetryEvent()
+    
+def main():
+    events = parse_file("example_data.json")
+    print(type(events))
+    print(events)
+
+if __name__ == "__main__":
+    main()
