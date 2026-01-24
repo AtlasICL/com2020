958807c07ff8f71ba01f819bd8b6fd9e73dbeace
email <ht555@exeter.ac.uk>
2026-01-24 20:28:57 +0000

    example plot in tkinter, scalable

---
+++ tel/telemetry_app/pyplotTk.py
@@ -0,0 +1,33 @@
+import numpy as np
+import tkinter as tk
+import seaborn as sns
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+
+root = tk.Tk()
+root.geometry('800x600')
+
+fig = plt.figure(1)
+plt.ion()
+sns.barplot(x=[1,2,3], y=[1,2,3])
+
+canvas = FigureCanvasTkAgg(fig, master=root)
+plot_widget = canvas.get_tk_widget()
+
+def update():
+    pass
+    # s = np.cos(np.pi*t)
+    # plt.plot(t,s)
+    # fig.canvas.draw()
+
+plot_widget.grid(row=0, column=0, sticky='nsew')
+tk.Button(root,text="Update",command=update).grid(row=1, column=0, sticky='ew')
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+root.mainloop()
