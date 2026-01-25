28a709b20d5536953faa893925e1c2420e63f673
Emre Acarsoy <dev@eacarsoy.com>
2026-01-25 17:43:21 +0000

    reorganised

--- tel/telemetry_app.py
+++
@@ -1,8 +0,0 @@
-from telemetry_app.gui import TelemetryAppGUI
-
-def main() -> None:
-    app = TelemetryAppGUI()
-    app.mainloop()
-
-if __name__ == '__main__':
-    main()
--- tel/telemetry_app/export_csv.py
+++
@@ -1,2 +0,0 @@
-class CSVExporter:
-    pass
