3ee8499130180888082f6417e440e4a0117501ec
Harry Taylor <ht555@exeter.ac.uk>
2026-01-29 02:11:11 +0000

    KillEnemyEvent and EnemyType enum

---
+++ game/src/main/java/WizardQuest/KillEnemyEvent.java
@@ -0,0 +1,36 @@
+package WizardQuest;
+
+public class KillEnemyEvent extends EncounterEvent{
+    private final EnemyType enemyType;
+    /**
+     * Constructor for KillEnemyEvent.
+     * 
+     * @param source        the object that constructed the telemetry event.
+     * @param userID        the ID of the user who is playing the game when the
+     *                      event is
+     *                      constructed.
+     * @param sessionID     the ID of the session the user is currently playing. See
+     *                      TelemetryListenerInterface for information about
+     *                      sessions.
+     * @param timeStamp     the time the event was constructed in the format
+     *                      yyyy/mm/dd/hh/mm/ss
+     * @param encounterName the name of the encounter a player is fighting.
+     * @param difficulty    the difficulty used for the players session.
+     * @param stageNumber   the stage player has completed.
+     * @param enemyType     type of enemy encounter.
+     */
+    public KillEnemyEvent(Object source, int userID, int sessionID, 
+            String timeStamp, EncounterType encounterName, Difficulty difficulty, int stageNumber, 
+            EnemyType enemyType){
+                super(source, userID, sessionID, timeStamp, "KillEnemy", encounterName, stageNumber, difficulty);
+                this.enemyType = enemyType;
+    }
+    /**
+     * Gets stored enemy type killed.
+     * 
+     * @return enemy type killed.
+     */
+    public EnemyType GetEnemyType(){
+        return this.enemyType;
+    }
+}
