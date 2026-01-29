7e71321592f8d63bbd16f0115118381d46d5f08d
Harry Taylor <ht555@exeter.ac.uk>
2026-01-29 20:23:57 +0000

    made BuyUpgradeEvent extend encounterEvent instead

--- game/src/main/java/WizardQuest/BuyUpgradeEvent.java
+++ game/src/main/java/WizardQuest/BuyUpgradeEvent.java
@@ -1,48 +1,38 @@
 package WizardQuest;
 
-public class BuyUpgradeEvent extends TelemetryEvent{
-    private final String difficulty;
-    private final int stageNumber;
+public class BuyUpgradeEvent extends EncounterEvent{
+    private final UpgradeType upgradeBought;
     private final int coinsSpent;
     /**
      * Constructor for BuyUpgradeEvent.
      * 
-     * @param source      the object that constructed the telemetry event.
-     * @param userID      the ID of the user who is playing the game when the
-     *                    event is
-     *                    constructed.
-     * @param sessionID   the ID of the session the user is currently playing. See
-     *                    TelemetryListenerInterface for information about
-     *                    sessions.
-     * @param timeStamp   the time the event was constructed in the format
-     *                    yyyy/mm/dd/hh/mm/ss
-     * @param difficulty  the difficulty used for the players session.
-     * @param stageNumber the stage player has completed.
-     * @param coinsSpent  the number of coins spent by the player on an upgrade.
+     * @param source        the object that constructed the telemetry event.
+     * @param userID        the ID of the user who is playing the game when the
+     *                      event is
+     *                      constructed.
+     * @param sessionID     the ID of the session the user is currently playing. See
+     *                      TelemetryListenerInterface for information about
+     *                      sessions.
+     * @param timeStamp     the time the event was constructed in the format
+     *                      yyyy/mm/dd/hh/mm/ss.
+     * @param difficulty    the difficulty used for the players session.
+     * @param stageNumber   the stage player has completed.
+     * @param upgradeBought the upgrade a player has purchased.
+     * @param coinsSpent    the number of coins spent by the player on an upgrade.
      */
-    //SUBJECT TO CHANGE
     public BuyUpgradeEvent(Object source, int userID, int sessionID, 
-        String timestamp, String difficulty, int stageNumber, int coinsSpent){
-        super(source, userID, sessionID, timestamp, "BuyUpgrade"); 
-        this.difficulty = difficulty;
-        this.stageNumber = stageNumber;
+        String timeStamp, EncounterType encounterName, Difficulty difficulty, int stageNumber, UpgradeType upgradeBought, int coinsSpent){
+        super(source, userID, sessionID, timeStamp, "BuyUpgrade", encounterName, difficulty, stageNumber);
+        this.upgradeBought = upgradeBought; 
         this.coinsSpent = coinsSpent;    
     }
     /**
-     * Gets the stored difficulty.
+     * Gets the stored upgrade bought.
      * 
-     * @return session difficulty.
+     * @return upgrade a player has purchased.
      */
-    public String GetDifficulty(){
-        return this.difficulty;
-    }
-    /**
-     * Gets the stored stage number.
-     * 
-     * @return current stage number.
-     */
-    public int GetStageNumber(){
-        return this.stageNumber;
+    public UpgradeType getUpgradeBought() {
+        return upgradeBought;
     }
     /**
      * Gets the stored number of coins spent.
