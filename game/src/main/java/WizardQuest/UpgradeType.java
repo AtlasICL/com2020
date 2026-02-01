ea0997e4eca082fc48f2241b6a4d3cf43835de23
wjf207 <wjf207@exeter.ac.uk>
2026-02-01 12:13:57 +0000

    Added unlock upgrades to UpgradeType

--- game/src/main/java/WizardQuest/UpgradeType.java
+++ game/src/main/java/WizardQuest/UpgradeType.java
@@ -1,15 +1,60 @@
 public enum UpgradeType{ //"Upgrade" is refered to as "passive ability externally"
     //organised as price, class, telemetry name
     //add abilities
-    PHYSICAL_DAMAGE_RESISTANCE(20, PhysicalDamageResistanceUpgrade.class, "PhysicalDamageResistance"),
-    FIRE_DAMAGE_RESISTANCE(10, FireDamageResistanceUpgrade.class, "FireDamageResistance"),
-    WATER_DAMAGE_RESISTANCE(5, WaterDamageResistanceUpgrade.class, "WaterDamageResistance"),
-    THUNDER_DAMAGE_RESISTANCE(5, ThunderDamageResistanceUpgrade.class, "ThunderDamageResistance"),
+    PHYSICAL_DAMAGE_RESISTANCE(
+        20, 
+        PhysicalDamageResistanceUpgrade.class, 
+        "PhysicalDamageResistance"
+    ),
+    FIRE_DAMAGE_RESISTANCE(
+        10, 
+        FireDamageResistanceUpgrade.class, 
+        "FireDamageResistance"
+    ),
+    WATER_DAMAGE_RESISTANCE(
+        5, 
+        WaterDamageResistanceUpgrade.class, 
+        "WaterDamageResistance"
+    ),
+    THUNDER_DAMAGE_RESISTANCE(
+        5, 
+        ThunderDamageResistanceUpgrade.class, 
+        "ThunderDamageResistance"
+    ),
     //MPROVED_PHYSICAL_DAMAGE(20, ImprovedPhysicalDamageUpgrade.class, "ImprovedPhysicalDamage"),
     //IMPROVED_FIRE_DAMAGE(15, ImprovedFireDamageUpgrade.class, "ImprovedFireDamage"),
     //IMPROVED_WATER_DAMAGE(10, ImprovedWaterDamageUpgrade.class, "ImprovedWaterDamage"),
     //IMPROVED_THUNDER_DAMAGE(10, ImprovedThunderDamageUpgrade.class, "ImprovedThunderDamage"),
-    FIRE_BALL_UNLOCK(30, FireBallUnlockUpgrade.class, "FireBallUnlock");
+    PUNCH_UNLOCK(
+        0, //not applicable
+        PunchUnlockUpgrade.class, 
+        "PunchUnlock"
+    ),
+    SLASH_UNLOCK(
+        15, 
+        SlashUnlockUpgrade.class, 
+        "SlashUnlock"
+    ),
+    ABSOLUTE_PULSE_UNLOCK(
+        10, 
+        AbsolutePulseUnlockUpgrade.class, 
+        "AbsolutePulseUnlock"
+    ),
+    WATER_JET_UNLOCK(
+        20, 
+        WaterJetUnlockUpgrade.class, 
+        "WaterJetUnlock"
+    ),
+    FIRE_BALL_UNLOCK(
+        30, 
+        FireBallUnlockUpgrade.class, 
+        "FireBallUnlock"
+    ),
+    THUNDER_STORM_UNLOCK(
+        25, 
+        ThunderStormUnlockUpgrade.class, 
+        "ThunderStormUnlock"
+    );
 
     private final int price;
     private final Class<? extends PlayerInterface> upgradeClass;
