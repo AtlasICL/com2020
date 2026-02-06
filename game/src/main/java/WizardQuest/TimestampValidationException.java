1c8c08d21357d13c41e9c71ae8130f4302828dc5
Harry Taylor <ht555@exeter.ac.uk>
2026-02-06 19:20:36 +0000

    required exceptions for telemetryListenerSingleton

---
+++ game/src/main/java/WizardQuest/TimestampValidationException.java
@@ -0,0 +1,34 @@
+package WizardQuest;
+
+/**
+ * Exception representing an issue relating to an incorrect time stamp for telemetry.
+ * The time stamp is in the future, or is of invalid format.
+ */
+public class TimestampValidationException extends Exception {
+    /**
+     * Throw the exception without a message or cause
+     */
+    public TimestampValidationException() {
+        super();
+    }
+
+    /**
+     * Throws the exception with a message to be displayed to the developer.
+     * 
+     * @param message the message to display.
+     */
+    public TimestampValidationException(String message) {
+        super(message);
+    }
+
+    /**
+     * Throws the exception with a message to be displayed to the developer and a
+     * cause for the exception being thrown.
+     * 
+     * @param message the message to display.
+     * @param cause   the cause for the exception.
+     */
+    public TimestampValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
