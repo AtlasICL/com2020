8dd836d1d078d193bc3ece0843b939b40cf8a05d
Emre Acarsoy <dev@eacarsoy.com>
2026-02-06 16:52:19 +0000

    wip

--- telemetry/auth/auth.py
+++ telemetry/auth/auth.py
@@ -1,10 +1,17 @@
 import os
+import requests
 import json
-from logto import LogtoClient, LogtoConfig
 
-ISSUER = os.environ.get("OIDC_ISSUER")
-CLIENT_ID = os.environ.get("OIDC_CLIENT_ID")
-CLIENT_SECRET = os.environ.get("OIDC_CLIENT_SECRET") 
+ISSUER = ""
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+
+def get_env_vars() -> None:
+    global ISSUER, CLIENT_ID, CLIENT_SECRET
+    ISSUER = os.environ.get("OIDC_ISSUER")
+    CLIENT_ID = os.environ.get("OIDC_CLIENT_ID")
+    CLIENT_SECRET = os.environ.get("OIDC_CLIENT_SECRET") 
+
 
 def validate_env_vars() -> None:
     if not ISSUER:
@@ -15,18 +22,25 @@ def validate_env_vars() -> None:
         raise KeyError("OIDC_CLIENT_SECRET environment variable is not set.")
     return
 
+
+def get_oauth_config():
+    url = str(ISSUER) + '/.well-known/openid-configuration'
+    req = requests.get(url=url, timeout=10)
+    return req.json()
+    
+
 def google_login():
     validate_env_vars()
-    client = LogtoClient(
-        LogtoConfig(
-            endpoint=ISSUER,
-            appId=CLIENT_ID,
-            appSecret=CLIENT_SECRET
-        )
-    )
+    oauth_config = get_oauth_config()
+    auth_endpoint = oauth_config["authorization_endpoint"]
+    token_endpoint = oauth_config["token_endpoint"]
+    
+
+
 
 def main():
     google_login()
+
 
 if __name__ == "__main__":
     main()
