86f3512fff31e3e314a1da278c6e6c7481ea72cb
Luca Pacitti <ljmp201@exeter.ac.uk>
2026-02-07 14:49:06 +0000

    Started implementation of unit tests

--- game/src/test/java/WizardQuest/unit/TelemetryListenerUnitTests.java
+++ game/src/test/java/WizardQuest/unit/TelemetryListenerUnitTests.java
@@ -1,20 +1,44 @@
 package WizardQuest.unit;
 
+import WizardQuest.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+
 public class TelemetryListenerUnitTests {
 
-    // NormalEncounterStartEvent will be used to test the behaviour of the abstract class TelemetryEvent,
-    // of which NormalEncounterStartEvent extends.
+    /**
+     * Creates a test user account with the role of Player, which will be used to independently execute each test.
+     * Also creates a mock event NormalEncounterStartEvent, which will be used to test the behaviour of the abstract
+     * class TelemetryEvent, of which it extends.
+     *
+     * @throws AuthenticationException if the username is already in use or is invalid when trying to create an account,
+     * or the credentials are invalid when trying to authenticate the user.
+     */
+    @BeforeEach
+    void setUp() throws AuthenticationException {
+        SettingsSingleton.getSettings().createNewUser("TestUser", "TestPassword", Role.PLAYER);
+        SettingsSingleton.getSettings().authenticateUser("TestUser", "TestPassword");
+        NormalEncounterStartEvent testEvent = new NormalEncounterStartEvent(null,
+                SettingsSingleton.getSettings().getUserID(),
+                SettingsSingleton.getSettings().getSessionID(),
+                TimeManagerSingleton.getTimeManager().convertDateTime(LocalDateTime.now()), // Fix this
+                "NormalEncounterStart",
+                "EncounterName",
+                1,
+                Difficulty.MEDIUM);
+    }
 
     /**
      * If the player is opted into telemetry, then telemetry events should be written to a JSON object as they occur.
      */
     @Test
     @DisplayName("TelemetryListener - Telemetry Event written to JSON if opted into telemetry")
-    void onNormalEncounterStart_telemetryWrittenIfOptedIn() {}
+    void onNormalEncounterStart_telemetryWrittenIfOptedIn() {
+    }
 
     /**
      * Alternatively, if the player has opted out of telemetry, then telemetry events should not be written to a JSON
