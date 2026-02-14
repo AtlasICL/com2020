3aa94390d84ef7f91aa5a5e8ee4963aa56634b19
Emre Acarsoy <dev@eacarsoy.com>
2026-02-14 01:24:22 +0000

    wip

--- game/src/main/java/WizardQuest/Authenticator.java
+++ game/src/main/java/WizardQuest/Authenticator.java
@@ -1,11 +1,34 @@
 package WizardQuest;
 
+import java.io.File;
+import java.lang.ProcessBuilder;
+
 public class Authenticator implements AuthenticatorInterface {
 
     @Override
     public AuthenticationResult login() {
+        
+        String name = "";
+        String userID = "";
+        RoleEnum role = RoleEnum.PLAYER;
 
+        // Instantiate the process builder, and set it up to be able
+        // to run the login python script.
+        ProcessBuilder procBuilder = new ProcessBuilder("python");
+        procBuilder.directory(new File("../telemetry/auth/"));
 
+        // We only want stdout not stderror
+        procBuilder.redirectErrorStream(false);
+
+        procBuilder.start();
+
+        // Parse the output ...
+
+        return new AuthenticationResult(
+            name, 
+            userID,
+            role
+        );
     }
 
 }
