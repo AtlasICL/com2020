0045430306e2559e62bdab02c4ce077b4540d15b
Emre Acarsoy <dev@eacarsoy.com>
2026-02-14 02:46:43 +0000

    wip - need to test

--- game/src/main/java/WizardQuest/Authenticator.java
+++ game/src/main/java/WizardQuest/Authenticator.java
@@ -1,12 +1,21 @@
 package WizardQuest;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.ProcessBuilder;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Authenticator implements AuthenticatorInterface {
 
     @Override
-    public AuthenticationResult login() {
+    public AuthenticationResult login() throws AuthenticationException {
         
         String name = "";
         String userID = "";
@@ -14,15 +23,45 @@ public class Authenticator implements AuthenticatorInterface {
 
         // Instantiate the process builder, and set it up to be able
         // to run the login python script.
-        ProcessBuilder procBuilder = new ProcessBuilder("python");
-        procBuilder.directory(new File("../telemetry/auth/"));
+        ProcessBuilder procBuilder = new ProcessBuilder(
+            "python",
+            "-m",
+            "auth.auth_wrapper"
+        );
+        procBuilder.directory(new File("../telemetry"));
 
         // We only want stdout not stderror
         procBuilder.redirectErrorStream(false);
 
-        procBuilder.start();
+        try {
+            Process proc = procBuilder.start();
 
-        // Parse the output ...
+            String output;
+            
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()))) {
+                output = reader.lines().collect(Collectors.joining());
+            } catch (IOException e) {
+                throw new AuthenticationException("Auth error." + e);
+            }
+
+            proc.waitFor();
+
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode json = mapper.readTree(output);
+                name = json.get("name").asText();
+                userID = json.get("sub").asText();
+                String roleVal = json.get("role").asText();
+                role = RoleEnum.valueOf(roleVal.toUpperCase());
+            } catch (JsonProcessingException e) {
+                throw new AuthenticationException("Auth error while parsing python login module output" + e); 
+            }
+
+        } catch (InterruptedException e) {
+            throw new AuthenticationException("Auth error while waiting for Python auth module response" + e);
+        } catch (IOException e) {
+            throw new AuthenticationException("Auth error occurred from Python process" + e);
+        }
 
         return new AuthenticationResult(
             name, 
