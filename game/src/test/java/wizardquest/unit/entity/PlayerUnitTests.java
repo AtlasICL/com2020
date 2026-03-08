00867885ee79bc63bbe2390bd85b86fd3bc3b8a5
Luca Pacitti <ljmp201@exeter.ac.uk>
2026-03-08 15:13:55 +0000

    Expanded * imports

--- game/src/test/java/wizardquest/unit/entity/PlayerUnitTests.java
+++ game/src/test/java/wizardquest/unit/entity/PlayerUnitTests.java
@@ -10,7 +10,9 @@ import wizardquest.entity.Player;
 import wizardquest.entity.PlayerInterface;
 import wizardquest.settings.DifficultyEnum;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlayerUnitTests {
 
