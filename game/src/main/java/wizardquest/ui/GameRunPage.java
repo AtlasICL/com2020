991676c871c9c295887ae336f51abbb2930cbaab
Luxeyo <106556703+Luxeyo@users.noreply.github.com>
2026-03-08 22:57:33 +0000

    Separated internal and external imports

--- game/src/main/java/wizardquest/ui/GameRunPage.java
+++ game/src/main/java/wizardquest/ui/GameRunPage.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 import wizardquest.auth.AuthenticationException;
 import wizardquest.auth.AuthenticationResult;
 import wizardquest.auth.Authenticator;
