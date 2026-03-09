34ed3c150722f3af30e6bbea61bd8e49b0ee164d
Emre Acarsoy <dev@eacarsoy.com>
2026-03-09 01:32:02 +0000

    Import ordering + more formatting

--- game/src/test/java/wizardquest/unit/entity/RandomEntityAIUnitTests.java
+++ game/src/test/java/wizardquest/unit/entity/RandomEntityAIUnitTests.java
@@ -1,37 +1,32 @@
 package wizardquest.unit.entity;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import wizardquest.entity.EntityAIInterface;
 import wizardquest.entity.EntityAISingleton;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class RandomEntityAIUnitTests {
 
     /**
      * The entity AI is tasked with randomly selecting targets to attack, abilities
-     * to use
-     * and upgrades to purchase. Each of the methods that implement these use the
-     * private
-     * method shuffleArray(), which uses generics to deal with arrays of any type.
+     * to use and upgrades to purchase. Each of the methods that implement these use
+     * the private method shuffleArray(), which uses generics to deal with arrays of
+     * any type.
      * This test runs this method on an Integer array of all ordered values 1-10
-     * twice,
-     * ensuring that the original differs from both shuffled arrays, and that the
-     * shuffled
-     * arrays are different from one another.
+     * twice, ensuring that the original differs from both shuffled arrays, and that
+     * the shuffled arrays are different from one another.
      *
      * @throws ClassNotFoundException    if the private class specified for
-     *                                   reflection cannot
-     *                                   be found.
+     *                                   reflection cannot be found.
      * @throws NoSuchMethodException     if the private method specified for
-     *                                   reflection cannot
-     *                                   be found.
+     *                                   reflection cannot be found.
      * @throws IllegalAccessException    if reflection fails.
      * @throws InvocationTargetException if reflection fails.
      */
@@ -58,7 +53,7 @@ public class RandomEntityAIUnitTests {
         // Invoke the shuffle method on both copies of the array.
         shuffleArray.invoke(entity, (Object) testArrCopy1);
         shuffleArray.invoke(entity, (Object) testArrCopy2);
-        
+
         // Probability of a pair of arrays below having the same order = 1/(10!) = 1 in
         // ~3.26 million
         assertFalse(Arrays.equals(testArrOriginal, testArrCopy1));
