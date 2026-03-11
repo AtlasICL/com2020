24e461e589d21abe461aeda5fb4caa7b41c660e8
wjf207 <wjf207@exeter.ac.uk>
2026-03-11 15:40:53 +0000

    implemented basic simulation interface

---
+++ game/src/main/java/wizardquest/gamemanager/SimulatedGameRun.java
@@ -0,0 +1,88 @@
+package wizardquest.gamemanager;
+
+import java.security.Timestamp;
+import java.time.LocalDateTime;
+
+import wizardquest.abilities.UpgradeEnum;
+import wizardquest.entity.PlayerInterface;
+import wizardquest.settings.DifficultyEnum;
+
+/**
+ */
+public class SimulatedGameRun implements GameRunInterface {
+
+    public SimulatedGameRun() {
+
+    }
+
+    @Override
+    public EncounterInterface pickEncounter() throws IllegalStateException {
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public UpgradeEnum[] viewShop() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void purchaseUpgrade(UpgradeEnum upgrade) throws LackingResourceException {
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public PlayerInterface getPlayer() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void nextStage() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getStage() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public LocalDateTime getRunStartTime() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getDeathCount() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void incrementDeathCount() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public DifficultyEnum getDifficulty() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getSessionID() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    private Timestamp getTimestamp() {
+
+        throw new UnsupportedOperationException();
+    }
+}
