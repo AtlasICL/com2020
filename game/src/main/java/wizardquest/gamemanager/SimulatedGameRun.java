8f919e7292e32ac5d1685adc497946a0e457dfdb
wjf207 <wjf207@exeter.ac.uk>
2026-03-11 17:12:53 +0000

    began implementation of simulated behaviour

--- game/src/main/java/wizardquest/gamemanager/SimulatedGameRun.java
+++ game/src/main/java/wizardquest/gamemanager/SimulatedGameRun.java
@@ -73,7 +73,188 @@ public class SimulatedGameRun implements GameRunInterface {
         this.random = new Random();
         this.deathCount = 0;
         this.sessionID = sessionID;
+        telemetryListener.setDestinationFile(new File("../event_logs/simulation_events.json"));
+
+        DifficultyEnum d = selectDifficulty();
+        gameManager.startNewGame(d);
+
+        GameRunInterface lastRun = null;
+        PlayerInterface lastPlayer = null;
+
+        EncounterInterface currentEncounter = gameManager.pickEncounter();
+
+        while (gameManager.isGameRunning()) {
+
+            try {
+                lastRun = gameManager.getCurrentRun();
+            } catch (Exception ignored) {}
+
+            try {
+                lastPlayer = gameManager.getCurrentPlayer();
+            } catch (Exception ignored) {}
+
+            boolean encounterWon = simulateEncounter(currentEncounter);
+
+            if (!encounterWon) {
+                continue;
+            }
+
+            GameRunInterface run = gameManager.getCurrentRun();
+
+            if (run != null && run.getStage() > 10) {
+                endScreen(lastRun, lastPlayer);
+                return;
+            }
+
+            simulateShop();
+
+            gameManager.advanceToNextLevel();
+
+            currentEncounter = gameManager.pickEncounter();
+        }
+
+        endScreen(lastRun, lastPlayer);
     }
+
+    @SuppressWarnings("CallToPrintStackTrace")
+    private boolean simulateEncounter(EncounterInterface encounter) {
+
+        GameRunInterface run = gameManager.getCurrentRun();
+
+        telemetryListener.onNormalEncounterStart(
+                new NormalEncounterStartEvent(
+                        settings.getUserID(), gameManager.getSessionID(),
+                        TimeManagerSingleton.getInstance().getCurrentTime(),
+                        encounter.getType(),
+                        gameManager.getCurrentDifficulty(),
+                        run != null ? run.getStage() : 1));
+
+        // Reset player at the start of the encounter
+        PlayerInterface player = gameManager.getCurrentPlayer();
+        if (player == null) {
+            gameManager.endGame();
+            return false;
+        }
+        player.resetHealth();
+        player.resetMagic();
+
+        // Encounter Loop
+        while (true) {
+
+            player.gainMagic(Math.min(player.getMagicRegenRate(), (player.getMaxMagic() - player.getMagic())));
+
+            EntityInterface[] enemies = encounter.getEnemies();
+
+            if (player.getHealth() <= 0) {
+                telemetryListener.onNormalEncounterFail(
+                        new NormalEncounterFailEvent(
+                                settings.getUserID(), gameManager.getSessionID(),
+                                timeManager.getCurrentTime(),
+                                encounter.getType(),
+                                gameManager.getCurrentDifficulty(),
+                                run != null ? run.getStage() : 1,
+                                player.getLives()));
+                gameManager.resetFailedEncounter();
+
+                if (player.getLives() == 0) {
+                    gameManager.endGame();
+                }
+
+                return false;
+            }
+
+            EntityInterface target = ai.pickTarget(enemies);
+            AbilityEnum ability = ai.pickAbility(player);
+
+            if (ability == null) {
+                gameManager.endGame();
+                return false;
+            }
+
+            if (target == null) {
+                gameManager.endGame();
+                return false;
+            }
+
+            try {
+                ability.execute(player, target);
+            } catch (LackingResourceException e) {
+                e.printStackTrace();
+            }
+
+            if (target.getHealth() <= 0) {
+                telemetryListener.onKillEnemy(
+                        new KillEnemyEvent(
+                                settings.getUserID(), gameManager.getSessionID(), timeManager.getCurrentTime(),
+                                encounter.getType(),
+                                gameManager.getCurrentDifficulty(),
+                                run != null ? run.getStage() : 1,
+                                target.getType()));
+            }
+
+            if (allEnemiesDead(enemies)) {
+                telemetryListener.onNormalEncounterComplete(
+                        new NormalEncounterCompleteEvent(
+                                settings.getUserID(), gameManager.getSessionID(), timeManager.getCurrentTime(),
+                                encounter.getType(),
+                                gameManager.getCurrentDifficulty(),
+                                run != null ? run.getStage() : 1,
+                                player.getHealth()));
+                gameManager.completeCurrentEncounter();
+                player.gainCoins(COINS_GAINED);
+                telemetryListener.onGainCoin(
+                        new GainCoinEvent(
+                                settings.getUserID(), gameManager.getSessionID(), timeManager.getCurrentTime(),
+                                encounter.getType(),
+                                gameManager.getCurrentDifficulty(),
+                                run != null ? run.getStage() : 1,
+                                COINS_GAINED));
+                return true;
+            }
+
+            // each enemy takes their turn
+            for (EntityInterface enemy : enemies) {
+                if (enemy == null)
+                    continue;
+                if (enemy.getHealth() <= 0)
+                    continue;
+
+                AbilityEnum a = ai.pickAbility(enemy);
+                try {
+                    a.execute(enemy, player);
+                } catch (LackingResourceException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    // Buys between 0 and 1 upgrades from the shop.
+    @SuppressWarnings("CallToPrintStackTrace")
+    private void simulateShop() {
+        UpgradeEnum[] upgrades = gameManager.viewShop();
+        UpgradeEnum u = ai.pickUpgrade(upgrades, gameManager.getCurrentPlayer().getCoins());
+        if (u != null) {
+            try {
+                gameManager.purchaseUpgrade(u);
+                telemetryListener.onBuyUpgrade(
+                        new BuyUpgradeEvent(
+                                settings.getUserID(),
+                                gameManager.getSessionID(),
+                                TimeManagerSingleton.getInstance().getCurrentTime(),
+                                gameManager.getCurrentEncounter() != null ? gameManager.getCurrentEncounter().getType()
+                                        : null,
+                                gameManager.getCurrentDifficulty(),
+                                gameManager.getCurrentRun() != null ? gameManager.getCurrentRun().getStage() : 1,
+                                u,
+                                u.getPrice()));
+            } catch (LackingResourceException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     @Override
     public EncounterInterface pickEncounter() throws IllegalStateException {
