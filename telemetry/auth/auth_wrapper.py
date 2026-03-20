6578b201b4dccea2488ac3f12944080d72d70fd2
wjf207 <wjf207@exeter.ac.uk>
2026-03-20 15:21:59 +0000

    Update auth_wrapper.py

--- telemetry/auth/auth_wrapper.py
+++ telemetry/auth/auth_wrapper.py
@@ -8,24 +8,31 @@ The wrapper is called directly (entry point at main()), and prints the
 results of the login call into stdout in json form.
 
 Please note that, much like the core auth module, this module requires
-the relevant environment variables to be set, namely OIDC_ISSUER, 
-OIDC_CLIENT_ID, and OIDC_CLIENT_SECRET.
+the shared OIDC config file at config/oidc.json.
 """
 
+import argparse
 import json
 
 from auth.auth import google_login
 
 
-def main():
-    sub, name, role = google_login()
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--oidc-config",
+        help="Path to the shared OIDC config file."
+    )
+    args = parser.parse_args()
+
+    sub, name, role = google_login(config_path=args.oidc_config)
     output = {
         "name": name,
         "sub": sub,
         "role": role.value
     }
     print(json.dumps(output))
-    return
+
 
 if __name__ == "__main__":
     main()
