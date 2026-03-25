624004639521bc2e3e20407ba112a958c46eef0e
Kazybek Khairulla <kazybekkhairulla@gmail.com>
2026-03-25 14:41:16 +0000

    added dependencies for java auth

--- game/src/main/java/module-info.java
+++ game/src/main/java/module-info.java
@@ -1,14 +1,18 @@
 module wizard.quest {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.net.http;
+    requires jdk.httpserver;
 
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jsr310;
-
+    requires java.sql;
     opens wizardquest.ui to javafx.fxml;
     opens wizardquest.telemetry;
     opens wizardquest.settings to com.fasterxml.jackson.databind;
     opens wizardquest.entity to com.fasterxml.jackson.databind;
 
     exports wizardquest.ui;
+    exports wizardquest.auth;
 }
